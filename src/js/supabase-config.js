ce7490e899c4edead54e94f78c3c5749001cf03e
lucio.dellafelice <lucio.dellafelice@origins-digital.com>
2024-12-19 10:20:11 +0100

    fix(database) fix supabase config

--- src/js/supabase-config.js
+++ src/js/supabase-config.js
@@ -1,5 +1,7 @@
-// Configuración de Supabase
-const SUPABASE_URL = 'https://phaavyhwbzmzfrndwkwb.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBoYWF2eWh3YnptemZybmR3a3diIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ1MzUwNDUsImV4cCI6MjA1MDExMTA0NX0.E0SttKH0ftlRy8tV5MfJQZw1kzF0yNnzCZ68OHIsn1c';
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
+export const supabase = createClient(supabaseUrl, supabaseKey)
+
